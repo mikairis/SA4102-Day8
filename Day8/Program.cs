2045f65a0f5601a8ce0819ddad2d3344803180d7
mikairis <thong.michael@gmail.com>
2017-08-15 11:38:08 +0800

    minor

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLibrary;
 
 namespace Day8
 {
+    /*class Person
+    {
+        public string name = "James";
+        public int age = 32;
+
+        public static void Walking()
+        {
+            Console.WriteLine("Walking.... Walking.....");
+        }
+    }*/
     class Program
     {
         static void Main(string[] args)
         {
+            /*Person one = new Person();
+            Console.WriteLine(one.name);
+            Console.WriteLine(one.age);
+            Console.WriteLine("and he is");
+            Person.Walking();
+            Console.ReadKey();*/
+            Abc.ToDo();
+            Console.ReadKey();
         }
     }
 }
